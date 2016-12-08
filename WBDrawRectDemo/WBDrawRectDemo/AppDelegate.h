cbef2566f81e04c684614ad00c7af7533e9d2f14
lsrain <315192042@qq.com>
2016-12-08 16:49:50 +0800

    project init

---
+++ WBDrawRectDemo/WBDrawRectDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WBDrawRectDemo
+//
+//  Created by WangBiao on 2016/12/8.
+//  Copyright © 2016年 lsrain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
